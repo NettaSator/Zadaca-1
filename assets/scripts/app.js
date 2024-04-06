fa49ed4a43895552569637c82441ad8fdc44fd44
NettaSator <165832841+NettaSator@users.noreply.github.com>
2024-04-06 23:57:41 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,46 @@ pocetnaVrijednost = pocetnaVrijednost + 5;
 
 outputResult(pocetnaVrijednost, "0+5");
 
+
+function zbirBrojeva(a, b) {
+  let zbir = a + b;
+  return zbir;
+}
+
+let rezultat = zbirBrojeva(1, 3);
+
+
+
+function razlikaBrojeva(a, b) {
+  let razlika = a - b;
+  return razlika;
+}
+
+let rezultat = razlikaBrojeva(4, 3);
+
+
+
+function zbirBrojeva(a, b) {
+  let zbir = a + b;
+  return zbir;
+}
+
+let rezultat = zbirBrojeva(1, 3);
+
+
+
+function proizvodBrojeva(a, b) {
+  let proizvod = a * b;
+  return proizvod;
+}
+
+let rezultat = proizvodBrojeva(2, 3);
+
+
+
+function kolicnikBrojeva(a, b) {
+  let kolicnik = a / b;
+  return kolicnik;
+}
+
+let rezultat = kolicnikBrojeva(1, 3);
